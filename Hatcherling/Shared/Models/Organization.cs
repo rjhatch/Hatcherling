60ca36f41a813e2503a069550b5df44f95a02b85
Richie <richdev378@gmail.com>
2022-06-20 12:19:58 -0600

    Added fields to Person and Organization, small changes to frontend.

--- Hatcherling/Shared/Models/Organization.cs
+++ Hatcherling/Shared/Models/Organization.cs
@@ -2,5 +2,6 @@
 public class Organization
 {
     public Guid Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; } = null!;
+    public ICollection<Person>? People { get; set; }
 }
--- Hatcherling/Shared/Models/Person.cs
+++ Hatcherling/Shared/Models/Person.cs
@@ -5,5 +5,8 @@ public class Person
     public Guid Id { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
-    public Guid OrganizationId { get; set; }
+    public byte[] PasswordHash { get; set; } = null!;
+    public byte[] PasswordSalt { get; set; } = null!;
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public ICollection<Organization> Organizations { get; set; } = null!;
 }
